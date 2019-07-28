6d87f4b6d9ef7d09d0360671c5129c56a5d2f6b9
arwahab <arw9511@gmail.com>
2019-07-28 18:40:11 -0400

    learning angular components

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    
+  <div *ngFor="let color of colors; even as e">
+    <h2>{{e}} {{color}}</h2>
+  </div>
+
+    `, 
+  styles: []
+})
+
+export class TestComponent implements OnInit {
+
+  public colors = ["red", "blue", "green", "yellow"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
